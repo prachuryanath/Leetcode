edd9cb839adc8c2a61540b953a908905f27ab16d
Prachurya Nath <prachuryanath00@gmail.com>
2022-06-24 23:18:54 +0530

    Time: 68 ms (39.23%), Space: 15.2 MB (16.71%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.py
+++ 412-fizz-buzz/412-fizz-buzz.py
@@ -1,14 +1,18 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         answer = []
-        for i in range(1,n+1):
-          if i%3 == 0:
-            if i%5 == 0:
-              answer.append('FizzBuzz')
-            else:
-              answer.append('Fizz')
-          elif i%5 == 0:
-            answer.append('Buzz')
-          else:
-            answer.append(str(i))
+        fizz_buzz = {3: "Fizz", 5: "Buzz"}
+        divisors = {3,5}
+        
+        for num in range(1,n+1):
+          num_ans_str = []
+          
+          for key in divisors:
+            if num%key ==0:
+              num_ans_str.append(fizz_buzz[key])
+              
+          if not num_ans_str:
+            num_ans_str.append(str(num))
+          answer.append(''.join(num_ans_str))
+          
         return answer
