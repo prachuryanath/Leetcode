f128a2a9a41aea43ea93c3179d53ddc3109c1824
Prachurya Nath <prachuryanath00@gmail.com>
2022-06-30 22:28:36 +0530

    Time: 182 ms (5.10%), Space: 15.4 MB (33.43%) - LeetHub

---
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minMoves2(self, nums: List[int]) -> int:
+        n = len (nums)
+        mid = n//2
+        nums.sort ()
+        res = 0
+        for i in range (n):
+            res = res + abs (nums [i] - nums [mid])
+        return res
