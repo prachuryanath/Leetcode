53f7ab1b9a234c4f440898a97f5f2ab33bb62393
Prachurya Nath <prachuryanath00@gmail.com>
2022-09-12 15:15:43 +0530

    Time: 8 ms (5.49%), Space: 6.4 MB (51.00%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+      stack<char>p;
+      for(int i=0;i<s.length();i++){
+        if(s[i]=='('|| s[i]=='{' || s[i]=='['){
+          p.push(s[i]);
+        }else if(s[i]==')'  && p.size() && p.top()=='('){
+          p.pop();
+        }
+        else if(s[i]=='}'  && p.size() && p.top()=='{'){
+          p.pop();
+        }
+        else if(s[i]==']'  && p.size() && p.top()=='['){
+          p.pop();
+        }
+        else{
+          return false;
+        }
+      }
+      return p.size()==0;
+    }
+};
